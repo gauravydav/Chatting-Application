67e02ca1daa933644e1e603a0a76d0cebf54cac3
gauravydav <gauravyadavry_cse18@its.edu.in>
2023-06-23 15:59:06 +0530

    Updates

---
+++ precache-manifest.d92ebb250255bc08aa435595f3aa651a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ebc79c6badcb3f32e42b9ef5b26b3f",
+    "url": "/chatting-app/index.html"
+  },
+  {
+    "revision": "9071ba9efc604a30bbf4",
+    "url": "/chatting-app/static/css/main.de005c1d.chunk.css"
+  },
+  {
+    "revision": "3c1acf78c80f056abba9",
+    "url": "/chatting-app/static/js/2.b2bd6ec7.chunk.js"
+  },
+  {
+    "revision": "edaf88c8aa6bedcdb624531641d638d8",
+    "url": "/chatting-app/static/js/2.b2bd6ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9071ba9efc604a30bbf4",
+    "url": "/chatting-app/static/js/main.0638b3f7.chunk.js"
+  },
+  {
+    "revision": "e5ce742e13dd98502e6c",
+    "url": "/chatting-app/static/js/runtime-main.6e7dfd30.js"
+  }
+]);
